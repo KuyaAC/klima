36817425017bbc3c2363566e095efd19f9928101
KuyaAC <aciesnet02@gmail.com>
2025-04-28 16:03:28 +0800

    Finishing API Endpoints

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,12 +1,58 @@
+import { API_CONFIG } from "./config"
+import { Coordinates, ForecastData, GeocodingResponse, WeatherData } from "./types";
+
 class WeatherAPI{
-    private createUR() {}
+    private createUrl(
+        endpoint: string,
+        params: Record<string, string | number>
+    ) {
+        cont searchParams = new URLSearchParams({
+            appid: API_CONFIG.API_KEY,
+            ...params,
+        });
 
-    private fetchData() {}
+        return `${endpoint}?${searchParams.toString()}`;
+    }
 
-    async getCurrentWeather() {}
+    private async fetchData<T>(url:string):Promise<T> {
+        const response = await fetch(url);
 
-    async getForecast() {}
+        if (!response.ok){
+            throw new Error(`Weather API Error: ${response.statusText}`);
+        }
+        return response.json();
+    }
 
-    async reverseGeocode() {}
+    async getCurrentWeather({lat,lon}:Coordinates):Promise<WeatherData> {
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+            lat: lat.toString(),
+            lon: lon.toString(),
+            units: API_CONFIG.DEFAULT_PARAMS.units,
+        });
+
+        return this.fetchData<WeatherData>(url);
+    }
+
+    async getForecast({lat,lon}:Coordinates):Promise<ForecastData> {
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+            lat: lat.toString(),
+            lon: lon.toString(),
+            units: API_CONFIG.DEFAULT_PARAMS.units,
+        });
+
+        return this.fetchData<ForecastData>(url);
+    }
+
+    async reverseGeocode({lat,lon,}:Coordinates):Promise<GeocodingResponse[]> {
+        const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
+            lat: lat.toString(),
+            lon: lon.toString(),
+            limit: 1,
+        });
+
+        return this.fetchData<GeocodingResponse[]>(url);
+    }
 
 }
+
+export const weatherAPI = new WeatherAPI();
