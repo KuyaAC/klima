d8a6375ac71be33bfb269b0919ca30e73f1c054f
KuyaAC <aciesnet02@gmail.com>
2025-05-06 20:10:58 +0800

    Done with the fetching

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -9,7 +9,7 @@ const WeatherDashboard = () => {
 const { coordinates, error: locationError, getLocation, isLoading: locationLoading} = useGeolocation();
 
   const weatherQuery = useWeatherQuery(coordinates);
-  const forecastnQuery = useForecastQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
   const locationQuery = useReverseGeocodeQuery(coordinates);
   
   
@@ -18,7 +18,7 @@ const { coordinates, error: locationError, getLocation, isLoading: locationLoadi
     getLocation()
     if(coordinates){
       weatherQuery.refetch()
-      forecastnQuery.refetch()
+      forecastQuery.refetch()
       locationQuery.refetch()
     }
   };
@@ -32,14 +32,14 @@ const { coordinates, error: locationError, getLocation, isLoading: locationLoadi
       <Alert variant="destructive">
       <AlertTriangle className="h-4 w-4" />
       <AlertTitle>Location Error</AlertTitle>
-      <AlertDescription className="flex flex-col gap-4">
-        <p>{locationError}</p>
-        <Button onClick={getLocation} variant={"outline"} className="w-fit">
-          <MapPin className="mr-2 h-4 w-4" />
-          Enable Location
-        </Button>
-      </AlertDescription>
-    </Alert>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
     );
   }
 
@@ -59,6 +59,28 @@ const { coordinates, error: locationError, getLocation, isLoading: locationLoadi
     );
   }
 
+  const locationName = locationQuery.data?.[0];
+
+  if(weatherQuery.error || forecastQuery.error){
+    return (
+      <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Failed to fetch weather data. Please try again.</p>
+          <Button onClick={handleRefresh} variant={"outline"} className="w-fit">
+            <RefreshCw className="mr-2 h-4 w-4" />
+              retry
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!weatherQuery.data || !forecastQuery.data){
+    return <WeatherSkeleton />;
+  }
+
   return (
     <div className="space-y-4">
       {/* Favorite Cities */}
@@ -68,14 +90,26 @@ const { coordinates, error: locationError, getLocation, isLoading: locationLoadi
         <Button
           variant={"outline"}
           size={"icon"}
-          // onclick={handleRefresh}
-          // disable={}
+          onclick={handleRefresh}
+          disabled={weatherQuery.isFetching || forecastQuery.isFetching}
           >
-            <RefreshCw className="h-4 w-4" />
+            <RefreshCw className={`h-4 w-4 ${weatherQuery.isFetching ? "animate-spin" : ""}`} />
         </Button>
       </div>
 
       {/* Current and Hourly Weather */}
+
+      <div>
+          <div>
+            {/* current weather */}
+            {/* hour temperature */}
+          </div>
+
+          <div>
+            {/* details  */}
+            {/* forecast  */}
+          </div>
+      </div>
 
     </div>
   )
