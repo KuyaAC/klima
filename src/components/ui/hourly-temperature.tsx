68ef97120255f004558b0c0b4faf71871bf11f81
KuyaAC <aciesnet02@gmail.com>
2025-05-07 19:56:03 +0800

    Hourly temperature DONE

--- src/components/ui/hourly-temperature.tsx
+++ src/components/ui/hourly-temperature.tsx
@@ -1,6 +1,13 @@
 import type { ForecastData } from "@/api/types";
 import { Card, CardContent, CardHeader, CardTitle } from "./card";
-import { Line, LineChart, ResponsiveContainer, XAxis } from "recharts";
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { format } from "date-fns";
 
 interface HourlyTemperatureProps {
@@ -29,6 +36,60 @@ const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
                 fontSize={12}
                 tickLine={false}
                 axisLine={false}
+              />
+              <YAxis
+                stroke="#888888"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={(value) => `${value}°`}
+              />
+              {/* tooltip */}
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="rounded-lg border bg-background p-2 shadow-sm">
+                        <div className="grid grid-cols-2 gap-4">
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              Temperature
+                            </span>
+                            <span className="font-bold">
+                              {payload[0].value}°
+                            </span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              Feels Like
+                            </span>
+                            <span className="font-bold">
+                              {payload[1].value}°
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                }}
+              />
+
+              {/* Line Part  */}
+              <Line
+                type="monotone"
+                dataKey="temp"
+                stroke="#2563eb"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                type="monotone"
+                dataKey="feels_like"
+                stroke="#64748b"
+                strokeWidth={2}
+                dot={false}
+                strokeDasharray="5 5"
               />
             </LineChart>
           </ResponsiveContainer>
