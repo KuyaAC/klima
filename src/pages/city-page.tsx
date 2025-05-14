750afe4595aabb5196b3d8fca9530e24c004e71b
KuyaAC <aciesnet02@gmail.com>
2025-05-14 15:08:28 +0800

    Start with the CityPage

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,6 +1,10 @@
+import { useParams, useSearchParams } from "react-router-dom";
 
 const CityPage = () => {
   return (
+    const [searchParams] = useSearchParams();
+    const params = useParams();
+
     <div>CityPage</div>
   )
 }
